fa880ba985c032b2d5b7cf81684be365c2f9f77b
PopYg <babotop11@naver.com>
2020-01-23 18:11:50 +0900

    20-01-23 pc버전 완료

--- public/ko/pc/dist/javascripts/design_common.js
+++ public/ko/pc/dist/javascripts/design_common.js
@@ -90,6 +90,21 @@ function companyJS(){
             }
         });
     });
+
+    //연혁
+    var $history = $(".history");
+    var $promotionTab = $history.find(".promotion_tab button");
+    var $historySec = $history.find(".history_box > section");
+
+    $promotionTab.on("click", function () {
+        var _this = $(this);
+        var _index = _this.parent().index();
+
+        $historySec.css({display:"none"});
+        $historySec.eq(_index).css({display:"block"});
+        $promotionTab.removeClass("active");
+        $promotionTab.eq(_index).addClass("active");
+    });
 }
 function etcJS(){
     var $utilTerms = $(".util_terms");
